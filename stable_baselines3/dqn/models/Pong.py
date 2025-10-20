3979a053edfcbeae6679a8e9c260faa4719cbcf0
Khanh <khanh.hxg@outlook.com>
2025-10-20 13:42:08 +0300

    fixed spelling mistake

--- stable_baselines3/dqn/models/Pong.py
+++ stable_baselines3/dqn/models/Pong.py
@@ -16,7 +16,6 @@ import gymnasium as gym
 import argparse
 
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--buffer', type=str2bool, default=True)
 parser.add_argument('--secondnet', type=str2bool, default=True)
@@ -27,6 +26,8 @@ args = parser.parse_args()
 
 
 def main(buffer: bool, secondnet: bool, prio: bool, duel: bool, num_env: int):
+
+    print(gym.envs.registry.keys())
     
     algo = algos(buffer, secondnet, prio, duel)
 
@@ -37,7 +38,7 @@ def main(buffer: bool, secondnet: bool, prio: bool, duel: bool, num_env: int):
     environment = VecFrameStack(DummyVecEnv([lambda: environment]), n_stack=4)
 
 
-    model = DQN('CnnPolicy', env=envs, batch_size=32, l1earning_starts=100000, train_freq=4, verbose=0, duel=False,
+    model = DQN('CnnPolicy', env=envs, batch_size=32, learning_starts=100000, train_freq=4, verbose=0, duel=duel,
                 target_update_interval=1000, gradient_steps=1, buffer_size=10000, use_buffer=buffer, use_second_net=secondnet, prio_replay=prio,
                 exploration_final_eps=0.01, exploration_fraction=0.1, gamma=0.99, learning_rate=0.0001, n_steps=10000000
               )
@@ -47,7 +48,7 @@ def main(buffer: bool, secondnet: bool, prio: bool, duel: bool, num_env: int):
     new_logger = Logger(folder=None, output_formats=[csv_out])
     model.set_logger(new_logger)
     evaluate = EvalCallback(environment, eval_freq=50, n_eval_episodes=10)
-    model.learn(total_timesteps=1200, callback=evaluate, log_interval=50)
+    model.learn(total_timesteps=1200000, callback=evaluate, log_interval=50)
 
 if __name__ == "__main__":
     main(args.buffer, args.secondnet, args.prio, args.duel, args.num_env)
